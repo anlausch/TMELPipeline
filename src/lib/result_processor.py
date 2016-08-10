633bd61a19a5613a027b14c19a22761c4bfbd636
anlausch <anne-lauscher@web.de>
2016-08-10 10:13:08 +0200

    [REFACTORING] add comments, clean code

--- src/lib/result_processor.py
+++ src/lib/result_processor.py
@@ -3,7 +3,6 @@ Created on 25.04.2016
 
 @author: anlausch
 '''
-from lib.database_connection import DatabaseConnection
 import codecs
 import settings as s
 
@@ -13,23 +12,36 @@ class ResultProcessor(object):
     This class provides post processing of the topic modeling output
     '''
 
-
     def __init__(self):
         '''
         Constructor
         '''
     
-    def getTopics(self, a, labels):
-        return [labels[int(elem)] for elem in a[1::2]]
+    def getTopics(self, line_list, label_list):
+        '''
+        Returns the corresponding label of every second element (=topic) in a given list starting by 1
+        @param {List} line_list, list that contains topic indices and proportions
+        @param {List} label_list, label index
+        '''
+        return [label_list[int(elem)] for elem in line_list[1::2]]
     
     
-    def getProportions(self, a):
-        return [float(elem) for elem in a[2::2]]
+    def getProportions(self, line_list):
+        '''
+        Returns every second element (=proportion) in a given list starting by 2
+        @param {List} line_list, list that contains topic indices and proportions
+        '''
+        return [float(elem) for elem in line_list[2::2]]
     
     
     def link_document_topic(self, database_connection):
+        '''
+        Links documents and topic labels by processing the label-index.txt 
+        and document-topic-distributions.csv output files of the stanford tmt
+        and inserts the information into the database
+        @param {DatabaseConnection} database_connection, connection to the db
+        '''
         label_index = [line.replace("\n","") for line in open(s.ROOT + "\\output\\llda-output\\01000\\label-index.txt").readlines()]
-        print(label_index)
         with open(s.ROOT + "\\output\\llda-output\\document-topic-distributions.csv", "r") as f:
             for line in f:
                 line_array = line.split(",")
@@ -37,11 +49,15 @@ class ResultProcessor(object):
                 topics = self.getTopics(line_array, label_index)
                 proportions = self.getProportions(line_array)
                 topic_distribution = [dict({"document": document, "topic": topics[i], "fraction": proportions[i]}) for i in range(0, len(topics))]
-                print(topic_distribution)
                 database_connection.insert_document_topic_distribution(topic_distribution)
     
     
     def link_topic_term(self, database_connection):
+        '''
+        Links topics (=entities) and terms by processing the summary.txt output file 
+        of the stanford tmt and inserts the information into the database
+        @param {DatabaseConnection} database_connection, connection to the db
+        '''
         file_lines = codecs.open(".\\..\\..\\output\\llda-output\\01000\\summary.txt", "r", "utf-8").readlines()
         is_label = True;
         for line in file_lines:
@@ -54,12 +70,3 @@ class ResultProcessor(object):
                 database_connection.insert_topic(record)
             elif line == "\n" or line == "\r\n":
                 is_label = True
-    
-    
-def main():
-    rp = ResultProcessor("LLDA")
-    dc = DatabaseConnection("EmailForensics", "root", "root")
-    rp.insert_topics(dc) 
-
-if __name__ == "__main__":
-    main()
